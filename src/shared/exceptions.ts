7eaf7920dddad240d73193a50188175d63a6775b
R-unic <roleyspersonal@gmail.com>
2023-04-26 17:11:06 -0700

    feat: add util type

--- src/shared/exceptions.ts
+++ src/shared/exceptions.ts
@@ -1,19 +1,17 @@
 import { $error } from "rbxts-transform-debug";
 
 export class Exception {
-  public constructor(
-    name: string,
-    public readonly message: string,
-    public readonly level?: number
-  ) {
-    throw $error(`${name}Exception: ${message}`, (level ?? 0) + 1);
-  }
+	public constructor(
+		name: string,
+		public readonly message: string,
+		public readonly level?: number
+	) {
+		throw $error(`${name}Exception: ${message}`, (level ?? 0) + 1);
+	}
 }
 
 export class FlameworkIgnitionException extends Exception {
-  public constructor(
-    public readonly message: string
-  ) {
-    super("FlameworkIgnition", message);
-  }
+	public constructor(public readonly message: string) {
+		super("FlameworkIgnition", message);
+	}
 }
