a4dfb3a0d4428416ca9fa5e2bbc99a5b7f07cff6
R-unic <roleyspersonal@gmail.com>
2024-02-27 02:08:00 -0500

    fix: update usage of Maids to Janitors

--- src/client/base-components/button-animation.ts
+++ src/client/base-components/button-animation.ts
@@ -1,5 +1,6 @@
 import { BaseComponent } from "@flamework/components";
 import { TweenInfoBuilder } from "@rbxts/builders";
+import { Janitor } from "@rbxts/janitor";
 
 interface Attributes {
   Hover?: boolean;
@@ -13,16 +14,17 @@ export default abstract class ButtonAnimation<
 
   protected readonly abstract tweenInfo: TweenInfoBuilder;
   protected readonly includeClick: boolean = true;
+  protected readonly janitor = new Janitor;
 
   protected abstract active(): void;
   protected abstract inactive(): void;
 
   public connectEvents(): void {
-    this.maid.GiveTask(this.instance.MouseEnter.Connect(() => this.active()));
-    this.maid.GiveTask(this.instance.MouseLeave.Connect(() => this.inactive()));
+    this.janitor.Add(this.instance.MouseEnter.Connect(() => this.active()));
+    this.janitor.Add(this.instance.MouseLeave.Connect(() => this.inactive()));
     if (this.includeClick) {
-      this.maid.GiveTask(this.instance.MouseButton1Down.Connect(() => this.inactive()));
-      this.maid.GiveTask(this.instance.MouseButton1Up.Connect(() => this.active()));
+      this.janitor.Add(this.instance.MouseButton1Down.Connect(() => this.inactive()));
+      this.janitor.Add(this.instance.MouseButton1Up.Connect(() => this.active()));
     }
   }
 }
