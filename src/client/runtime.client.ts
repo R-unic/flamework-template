4cf7712ff0a4ec20cca6b3d7f43fdc51fffaa4f9
R-unic <roleyspersonal@gmail.com>
2024-04-04 16:06:53 -0400

    feat: create Version dependency & register custom dependencies in runtime scripts

--- src/client/runtime.client.ts
+++ src/client/runtime.client.ts
@@ -1,7 +1,9 @@
 import { Flamework } from "@flamework/core";
 import { FlameworkIgnitionException } from "shared/exceptions";
+import * as Dependencies from "shared/utility/dependencies";
 
 try {
+	Dependencies.registerAll();
 	Flamework.addPaths("src/client/hook-managers");
 	Flamework.addPaths("src/client/components");
 	Flamework.addPaths("src/client/controllers");
--- src/server/runtime.server.ts
+++ src/server/runtime.server.ts
@@ -1,7 +1,9 @@
 import { Flamework } from "@flamework/core";
 import { FlameworkIgnitionException } from "shared/exceptions";
+import * as Dependencies from "shared/utility/dependencies";
 
 try {
+	Dependencies.registerAll();
 	Flamework.addPaths("src/server/hook-managers");
 	Flamework.addPaths("src/server/components");
 	Flamework.addPaths("src/server/services");
