b659fd9cb15cdc7430ee86c6827e86a1b512d652
R-unic <roleyspersonal@gmail.com>
2024-04-22 06:45:34 -0400

    feat: create 5 more string utility functions

--- src/shared/utility/strings.ts
+++ src/shared/utility/strings.ts
@@ -1,7 +1,45 @@
 import { slice } from "@rbxts/string-utils";
 
+/**
+ * Takes a slug-string and converts it into PascalCase
+ */
 export function slugToPascal(slug: string): string {
   return slug.split("-")
     .map(word => word.sub(1, 1).upper() + slice(word, 1))
     .join("");
+}
+
+/**
+ * Takes a camelCase string and converts it into spaced text
+ */
+export function camelCaseToSpaced(camelCased: string): string {
+  return camelCased.gsub("%u", " %1")[0];
+}
+
+/**
+ * Takes a PascalCase and converts it into paced text
+ */
+export function pascalCaseToSpaced(pascalCased: string): string {
+  return camelCaseToSpaced(pascalCased).sub(2);
+}
+
+/**
+ * Capitalizes the first letter of `text`
+ */
+export function capitalize(text: string): string {
+  return text.gsub("^%1", s => s.upper())[0];
+}
+
+/**
+ * Removes all whitespace characters from `text`
+ */
+export function removeWhitespace(text: string): string {
+  return text.gsub("%s+", "")[0];
+}
+
+/**
+ * Trims all leading & trailing whitespace characters from `text`
+ */
+export function trim(text: string): string {
+  return text.gsub("^%s*(.-)%s*$", "%1")[0];
 }
