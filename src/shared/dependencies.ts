9a0659af0a50f261ba15dab2476475a391bedecf
R-unic <roleyspersonal@gmail.com>
2024-05-04 22:30:40 -0400

    feat: create Singleton decorator

--- src/shared/dependencies.ts
+++ src/shared/dependencies.ts
@@ -1,4 +1,4 @@
-import { Modding } from "@flamework/core";
+import { Modding, Reflect } from "@flamework/core";
 
 // This file is for dependencies that aren't singletons or components
 // This type is to prevent type interning from TypeScript
@@ -10,6 +10,14 @@ type Marker = {
  * The name of the dependency `Name` was injected into
  */
 export type Name = string & Marker;
+
+/**
+ * Request the required metadata for lifecycle events and dependency resolution.
+ * @metadata flamework:implements flamework:parameters injectable
+ */
+export const Singleton = Modding.createDecorator("Class", descriptor => {
+  Reflect.defineMetadata(descriptor.object, "flamework:singleton", true);
+});
 
 export function registerAll(): void {
   Modding.registerDependency<Name>(ctor => tostring(ctor));
