65bdc1b54a368e0158d913f35df732e2345c0120
Riley Peel <49625808+R-unic@users.noreply.github.com>
2024-08-11 15:23:44 -0400

    perf: use buffer for StringBuilder class

--- src/shared/classes/string-builder.ts
+++ src/shared/classes/string-builder.ts
@@ -1,62 +1,43 @@
 export default class StringBuilder {
-  public indentation = 0;
-  private readonly parts: string[] = [];
+  private buf: buffer;
 
   /**
    * @param tabSize The amount of spaces a "tab" character will take up
    */
-  public constructor(
-    private readonly tabSize = 4
-  ) {}
+  public constructor(initial?: string) {
+    this.buf = initial !== undefined ? buffer.fromstring(initial) : buffer.create(0);
+  }
 
   /**
    * All of the parts combined
    */
-  public string(): string {
-    return this.parts.join("");
+  public toString(): string {
+    return buffer.tostring(this.buf);
   }
 
   /**
    * Appends pieces of strings onto the current result
    */
-  public append(...strings: string[]): void {
-    for (const str of strings)
-      this.parts.push(str);
-  }
-
-  /**
-   * Retrieves the last string piece appended
-   */
-  public peekLastPart(): string {
-    return this.parts[this.parts.size() - 1];
-  }
-
-  /**
-   * Removes the last string piece appended
-   */
-  public popLastPart(): string | undefined {
-    return this.parts.pop();
-  }
-
-  /**
-   * Add indentation for the next lines appended
-   */
-  public pushIndentation(): void {
-    this.indentation++;
-  }
-
-  /**
-   * Remove indentation for the next lines appended
-   */
-  public popIndentation(): void {
-    this.indentation--;
+  public append(text: string): void {
+    const offset = buffer.len(this.buf);
+    this.ensureSize(offset + text.size());
+    buffer.writestring(this.buf, offset, text, text.size());
   }
 
   /**
    * Appends new lines
    * @param amount The amount of new lines to append, defaults to 1
    */
-  public newLine(amount = 1): void {
-    this.append(("\n" + " ".rep(this.tabSize).rep(this.indentation)).rep(amount));
+  public appendLine(text: string): void {
+    this.append(text + "\n");
+  }
+
+  private ensureSize(minimumSize: number): void {
+    const bufferSize = buffer.len(this.buf);
+    if (minimumSize <= bufferSize) return;
+
+    const newBuf = buffer.create(minimumSize);
+    buffer.copy(newBuf, 0, this.buf, 0, bufferSize);
+    this.buf = newBuf;
   }
 }
