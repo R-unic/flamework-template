7c1977f57b45a1d3f639a93602a80a868ae269c9
R-unic <roleyspersonal@gmail.com>
2024-10-30 23:36:09 -0400

    chore: clean up garbage

--- src/shared/structs/packets.ts
+++ src/shared/structs/packets.ts
@@ -1,10 +1,5 @@
 import type { PlayerData } from "shared/data-models/player-data";
 
-export interface DataUpdatePacket {
-  readonly directory: string;
-  readonly value: unknown;
-}
-
 export interface PlayerDataPacket {
   readonly data: PlayerData;
 }
