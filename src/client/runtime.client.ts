58a479e94a3018f7ca6b07fa80c998f3a1f0f085
R-unic <roleyspersonal@gmail.com>
2024-11-26 22:20:53 -0500

    feat: create Multiple utility function to emulate multiple inheritance

--- src/client/runtime.client.ts
+++ src/client/runtime.client.ts
@@ -1,6 +1,9 @@
 import { Flamework } from "@flamework/core";
+import Destroyable from "shared/classes/destroyable";
+import { CumulativeID } from "shared/classes/id";
 
 import { FlameworkIgnited } from "shared/constants";
+import { Multiple } from "shared/utility/meta";
 
 try {
 	Flamework.addPaths("src/client/hook-managers");
@@ -11,3 +14,23 @@ try {
 } catch (e) {
 	error("Issue igniting Flamework: " + <string>e);
 }
+
+class Test extends Multiple(Destroyable, CumulativeID) {
+	public constructor() {
+		super();
+		this.janitor.Add(() => print("destroyed"))
+	}
+}
+
+class Test2 extends Test { }
+
+const test = new Test();
+test.destroy()
+print(test.id)
+print(test instanceof Destroyable)
+print(test instanceof CumulativeID)
+print(test instanceof Test)
+
+const test2 = new Test2();
+print(test2 instanceof Test2)
+print(test2 instanceof Test)
--- src/shared/utility/meta.ts
+++ src/shared/utility/meta.ts
@@ -3,8 +3,39 @@ import { getIdFromSpecifier } from "@flamework/components/out/utility";
 import type { Constructor } from "@flamework/core/out/utility";
 import type { Components } from "@flamework/components";
 
-declare const newproxy: <T extends symbol = symbol>(addMetatable: boolean) => T;
+function assign<T>(target: T, source: object, ...args: unknown[]): T {
+  for (const [key, value] of pairs(<Record<string, defined>>source))
+    (<Record<string, defined>>target)[key] = value;
 
+  if ("constructor" in source)
+    (<Callback>source.constructor)(target, ...args);
+
+  return target;
+}
+
+/**
+ * Combines multiple classes into one to emulate multiple inheritance, use with caution
+ *
+ * Note: `instanceof` may not work as expected when using this function
+ * */
+export function Multiple<BaseClasses extends Constructor[]>(
+  ...baseClasses: BaseClasses
+): Constructor<UnionToIntersection<InstanceType<BaseClasses[number]>>> {
+  return baseClasses.reduce(
+    (Combined, Current) => class extends Combined {
+      public constructor(...args: never[]) {
+        super(...args);
+        assign(this, new Current(...args));
+        assign(this, Current);
+      }
+    },
+    class {
+      public constructor(..._: never[]) { }
+    }
+  ) as Constructor<UnionToIntersection<InstanceType<BaseClasses[number]>>>;
+}
+
+/** Resolves all dependencies behind the `ctor` constructor */
 export function resolveDependency<T extends object = object>(ctor: Constructor<T>): T | T[] {
   try {
     return <T>Modding.resolveSingleton<T>(ctor);
@@ -14,6 +45,7 @@ export function resolveDependency<T extends object = object>(ctor: Constructor<T
   }
 }
 
+/** Calls `process` for every dependency resolved from `ctor` */
 export function processDependency<T extends object = object, O = void>(ctor: Constructor<T>, process: (dependency: T) => O): O {
   const dependencies = resolveDependency<T>(ctor);
   const isArray = (<T[]>dependencies).size() > 0;
@@ -28,10 +60,14 @@ export function processDependency<T extends object = object, O = void>(ctor: Con
   return process(<T>dependencies);
 }
 
+/** Calls the descriptor method for every dependency resolved from `ctor` */
 export function callMethodOnDependency<Args extends unknown[], O = void>(ctor: object, descriptor: MethodDescriptor<(self: unknown, ...args: Args) => O>, ...args: Args): O {
   return processDependency(<Constructor>ctor, dependency => descriptor.value(dependency, ...args))
 }
 
+declare const newproxy: <T extends symbol = symbol>(addMetatable: boolean) => T;
+
+/** Create a unique symbol */
 export function createSymbol<T extends symbol = symbol>(name: string): T {
   const symbol = newproxy<T>(true);
   const mt = <Record<string, unknown>>getmetatable(<never>symbol);
@@ -40,12 +76,12 @@ export function createSymbol<T extends symbol = symbol>(name: string): T {
 }
 
 /**
- * Generates a [mapping] decorator, and map, for the given object type and constructor arguments
+ * Generates a map and a decorator that adds the target to the map, for the given object type and constructor arguments
  */
 export function createMappingDecorator<T extends object, Args extends unknown[]>() {
   type ObjectConstructor = new (...args: Args) => T;
 
-  const map = new Map<string, [ObjectConstructor, Args]>();
+  const map = new Map<string, [ObjectConstructor, Args]>;
   const decorator = (...args: Args) => <K extends ObjectConstructor>(ctor: K) => void map.set(tostring(ctor), [ctor, args]);
   return <const>[map, decorator];
 }
