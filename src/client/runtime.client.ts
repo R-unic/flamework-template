2af08bad9100e056b4c07079def74b6d3948b313
R-unic <roleyspersonal@gmail.com>
2025-02-26 18:51:32 -0500

    chore: remove garbage from client runtime file

--- src/client/runtime.client.ts
+++ src/client/runtime.client.ts
@@ -1,19 +1,14 @@
 import { Flamework } from "@flamework/core";
-import { Workspace as World } from "@rbxts/services";
 
-import { Assets, FlameworkIgnited } from "shared/constants";
+import { FlameworkIgnited } from "shared/constants";
 
 try {
-	Flamework.addPaths("src/client/hook-managers");
-	Flamework.addPaths("src/client/components");
-	Flamework.addPaths("src/client/controllers");
-	Flamework.ignite();
+  Flamework.addPaths("src/client/hook-managers");
+  Flamework.addPaths("src/client/components");
+  Flamework.addPaths("src/client/controllers");
+  Flamework.ignite();
 
-	FlameworkIgnited.Fire();
+  FlameworkIgnited.Fire();
 } catch (e) {
-	error("Issue igniting Flamework: " + <string>e);
+  error("Issue igniting Flamework: " + <string>e);
 }
-
-const block = Assets.GrassBlock.Clone();
-block.Parent = World;
-block.Position = new Vector3(0, 4, 0);
